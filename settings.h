554da89c338f376c736de8bb1faf6fb8ab9834c8
Morti123 <114169876+Morti123@users.noreply.github.com>
2022-12-04 19:10:12 +0300

    Add files via upload

---
+++ settings.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+const float w_width = 800;
+const float w_height = 600;
+const std::string w_title = "Work_3"; 
+
+const float x = 20.f;
+const float y = 100.f;
+const float rize = 50.f;
+const sf::Color LEFT_BAT_COLOR(sf::Color::White);
+const sf::Color RIGHT_BAT_COLOR(sf::Color::White);
+const sf::Color Circle_Color (sf::Color::Black);
+const float r = 25.f;
+ float dx = 1.f;
+ float dy = 1.f;
+
